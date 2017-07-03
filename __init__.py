1ded9fb28483be6cdde5921ebf9bfe4d3957d39a
Hernando Nerwin <nerwin.hernando@nokia.com>
2017-07-03 14:59:12 +0800

    initial project

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+
+# instantiate the app
+app = Flask(__name__)
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
