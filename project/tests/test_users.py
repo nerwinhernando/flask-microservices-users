cf057266c2d16d161f127cd2a05a8f18659a4b7d
Hernando Nerwin <nerwin.hernando@nokia.com>
2017-07-04 19:38:27 +0800

    Restful routes: test 2

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -2,8 +2,9 @@
 
 
 import json
-
 from project.tests.base import BaseTestCase
+from project import db
+from project.api.models import User
 
 
 class TestUserService(BaseTestCase):
@@ -82,3 +83,36 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn('Sorry. That email already exists.', data['message'])
         self.assertIn('fail', data['status'])
+
+    #docker-compose run users-service python manage.py test
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = User(username='michael', email='michael@realpython.com')
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('created_at' in data['data'])
+            self.assertIn('michael', data['data']['username'])
+            self.assertIn('michael@realpython.com', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
